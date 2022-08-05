4aa96b4ebaf04fdb9e58fe0964ae377013957648
Giancarlo Tupac Gutierrez <47062199+GiancarloWalk@users.noreply.github.com>
2022-08-05 09:54:13 -0500

    Update variables.tf
    update default values

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "ibm_cloud_api_key" {
   description = "API Key to use."
-  default     = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  default     = "4KatVJLEJ2LbrvFRlgtZat7ecR6EffZe4dAzEsnArHIx"
 }
 variable "crn" {
   type        = string
-  default     = "crn:v1:staging:public:power-iaas:dal12:a/efe5e8b9d3f04b948790fe5499bd18bc:6021a723-bcab-4d3f-9985-d0cb2f864f35::"
+  default     = "crn:v1:bluemix:public:power-iaas:us-south:a/5d01a9061a5c411b92c33796e75e4466:0da7021e-a7fd-43de-9278-18d4dc2226b0::"
   description = "Power Systems Virtual Server CRN"
 }
 variable "image_name" {
@@ -23,7 +23,7 @@ variable "processors" {
 }
 variable "instance_name" {
   type        = string
-  default     = "paytesteruser1"
+  default     = "paytesteruser1-v2"
   description = "The name to assign to the instance"
 }
 variable "processor_type" {
@@ -43,16 +43,16 @@ variable "storage_type" {
 }
 variable "ssh_key_name" {
   type        = string
-  default = "paytestersshkey1"
+  default = "paytestersshkey1-v2"
   description = "The name of the public SSH RSA key to use when creating the instance, as defined for the selected Power Systems Virtual Server CRN"
 }
 variable "ssh_key" {
   type        = string
-  default = "ssh-rsa xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDWmXrndpQuOJbAD7YZ/tP3llHwddBjxc5cG1CyUXzuq0Wd0OXb/tHVsS1D9xwFdyVxel036CBIlqIUEEwLKd7Qc6rnMdBBOFIjQf/GVLoLp5EUV0xRfTxcSFYyW2wTpk50T/3YmoqNN15Tb+bbXH2OtyoeziMmdocwzzwyaWMaUleGlWuYYnLCcR3FiSMPaV+gPAoj6u0zlV19VUlUdZT3uQYjFP8Sq4u1gwb/uIo8IZaQKXtFmuOX3Q2RSVvB/L7OD3ZuMjtz9/RfI6KqSXQGDin1NAjjjxBnBOqtKzfgQOT/zvEcKvLNRY3+YLQ7fM4+Zm2XjFvxZSYTNxTMfi7jes7NeSyhoYVeS/OkHvp4cMi79bNQYalZQ9IVzBXF8duzM0xGAz2NB3RNOqbAqgdCBbSF5GKul/V7Ya0sh98DzoiTM3YcK4bew4Ryt5wCY5tB3hDUEDMXFdPMEr9VWMrCv3uJPhWwuq7mgvoyJUCLwaHzV0WH6WQSwlihSCQ02lU= gtupac@Giancarlos-MacBook-Pro-307.local"
   description = "The name of the public SSH RSA key to use when creating the instance, as defined for the selected Power Systems Virtual Server CRN"
 }
-variable "network_name" {
-  type        = string
-  default = "paytester-sn1"
-  description = "The network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
-}
+#variable "network_name" {
+#  type        = string
+#  default = "paytester-sn1"
+#  description = "The network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
+#}
